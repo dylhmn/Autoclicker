a91fbb224639e111af4c8931e1f78526f15c1383
dylhmn <dylharman.18@gmail.com>
2025-03-19 12:08:12 +0000

    Clarity in code and for users

--- Program.cs
+++ Program.cs
@@ -1,43 +1,47 @@
-﻿using System.Runtime.InteropServices;
-// For DLL (Dynamic-link library)
-
-// IMPORTS - Because C# doesn't have mouse events
-[DllImport("user32.dll")] // User 32 module (Windows API)
-static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dwExtraInfo); // click
+﻿using System.Runtime.InteropServices; // For DLL
+// IMPORTS
+[DllImport("user32.dll")] // Windows API
+static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dwExtraInfo); // Click
 [DllImport("user32.dll")]
-static extern short GetAsyncKeyState(int vKey); // hotkey
-
-const uint LEFTDOWN = 0x02; // LEFTDOWN = Left click
-const uint LEFTUP = 0x04; // LEFTUP = Releasing left click
+static extern short GetAsyncKeyState(int vKey); // Hotkey
+const uint LEFTDOWN = 0x02;
+const uint LEFTUP = 0x04;
+const int HOTKEY = 0x26;
 //(https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes)
 
-const int HOTKEY = 0x26; // Up arrow key
+bool enableClicker = false;
+int clickInterval = 5; // Miliseconds 
 
-bool enableClicker = false; // ON/OFF for hotkey
-int clickInterval = 5; // Miliseconds between clicks
+Console.Title = " ** AUTOCLICKER **";
+Console.Write("WELCOME!\nTHE CURRENT HOTKEY IS: ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("UP ARROW KEY");
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("\nINTERVAL: ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write(clickInterval.ToString());
 
-Console.WriteLine("Welcome! The current hotkey is the Up arrow key");
 // AUTOCLICKER LOOP
 while (true)
 {
-    if (GetAsyncKeyState(HOTKEY) < 0) // if hotkey is down
+    if (GetAsyncKeyState(HOTKEY) < 0)
     {
-        Console.Clear(); // Prevents clogging the terminal
+        Console.Clear();
         enableClicker = !enableClicker; // Enable / Disable vice versa
-        Console.WriteLine(enableClicker ? "Autoclicker ACTIVE" : "Autoclicker DISABLED");
+        Console.ForegroundColor = enableClicker ? ConsoleColor.Green : ConsoleColor.Red;
+        Console.WriteLine(enableClicker ? " + ACTIVE" : " - DISABLED");
         Thread.Sleep(300); // Stops spam
     }
     if (enableClicker)
     {
         MouseClick();
-
     }
     Thread.Sleep(clickInterval);
 }
-// MOUSE CLICK FUNCTION
-void MouseClick()
+
+void MouseClick() // MOUSE CLICK FUNCTION
 {
     mouse_event(LEFTDOWN, 0, 0, 0, IntPtr.Zero);
-    // No more information is needed than the click (No mouse movements)
     mouse_event(LEFTUP, 0, 0, 0, IntPtr.Zero);
+    // 0,0,0 = no mouse movements
 }
